e417c9a595338bb8f2c753a43067fc513f48ab04
Thomas <hi@thomasng.me>
2025-12-03 09:44:53 -0500

    D2P1

--- d2/p1.go
+++ d2/p1.go
@@ -3,7 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
+	// "io"
 	"log"
 	"os"
 	"strconv"
@@ -11,58 +11,52 @@ import (
 )
 
 type Solution struct {
-	invalids []int
+	invalids []int64
 }
 
 func solve(s *Solution, l string) {
-	// Remove trailing comma
-	l = l[:len(l)-1]
-
 	fromTo := strings.Split(l, "-")
 
-	from, err := strconv.Atoi(fromTo[0])
+	from, err := strconv.ParseInt(fromTo[0], 10, 64)
 
 	if err != nil {
 		log.Fatalf("Fatal error doing atoi: %s\n", err)
 	}
 
-	to, err := strconv.Atoi(fromTo[1])
+	to, err := strconv.ParseInt(fromTo[1], 10, 64)
 
 	if err != nil {
 		log.Fatalf("Fatal error doing atoi: %s\n", err)
 	}
 
-	i := from
-
-	for {
-		if i > to {
-			break
-		}
-
-		str := strconv.Itoa(i)
+	for i := from; i <= to; i++ {
+		str := strconv.FormatInt(i, 10)
 
 		repeat := isAllRepeat(str)
 		if repeat {
 			s.invalids = append(s.invalids, i)
-			fmt.Printf("%d\n", i)
-			i++
+			// fmt.Printf("%d\n", i)
 			continue
 		}
 
 		if str[:len(str)/2] == str[len(str)/2:] {
 			s.invalids = append(s.invalids, i)
-			fmt.Printf("%d\n", i)
+			// fmt.Printf("%d\n", i)
+			continue
 		}
-
-		i++
 	}
 }
 
 func isAllRepeat(s string) bool {
-	isSame := true
 	if len(s) == 1 {
-		return true
+		return false
 	}
+
+	if (len(s) % 2) == 1 {
+		return false
+	}
+
+	isSame := true
 
 	for i := range len(s) - 1 {
 		if s[i] == s[i+1] {
@@ -72,40 +66,26 @@ func isAllRepeat(s string) bool {
 		break
 	}
 
-	if len(s)%2 == 1 {
-		return false
-	}
-
 	return isSame
 }
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 
-	sln := Solution{}
+	sln := Solution{invalids: []int64{}}
 
-	for {
-		l, err := reader.ReadString(',')
+	text, _ := reader.ReadString('\n')
 
-		if err == io.EOF {
-			break
-		}
+	list := strings.Split(text, ",")
+	for i := 0; i < len(list); i++ {
+		// Remove trailing whitespace
+		l := strings.Trim(list[i], "\n")
 
-		if err != nil {
-			log.Fatalf("Fatal error reading stdin: %s\n", err)
-		}
-
-		l = strings.TrimSpace(l)
-
-		if len(l) == 0 {
-			continue
-		}
-
+		fmt.Println(l)
 		solve(&sln, l)
 	}
 
-	// fmt.Printf("Solution\n", sln.)
-	n := 0
+	var n int64 = 0
 	for i := 0; i < len(sln.invalids); i++ {
 		n += sln.invalids[i]
 	}
