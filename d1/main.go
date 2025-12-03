cbe00d33f5e3102b9c47f4ddf91da890ff8701f3
Thomas <hi@thomasng.me>
2025-12-03 00:32:27 +0000

    Working day 1 part 2

--- d1/main.go
+++ d1/main.go
@@ -26,9 +26,44 @@ func solve(s *Solution, l string) {
 	}
 
 	fmt.Println("////")
+	fmt.Printf("Zeroes: %d\n", s.zeroes)
 	fmt.Printf("Instruction: %s\n", l)
-	fmt.Printf("Ticks: %d\n", ticks)
 	fmt.Printf("Start Position: %d\n", s.position)
+
+	i := 1
+
+	for {
+		if right {
+			s.position += 1
+		} else {
+			s.position -= 1
+		}
+
+		if s.position == -1 {
+			s.position = 99
+		}
+
+		if s.position == 100 {
+			s.position = 0
+		}
+
+		if s.position == 0 {
+			s.zeroes += 1
+		}
+
+		if i == ticks {
+			break
+		}
+
+		i++
+	}
+
+	fmt.Printf("End Position: %d\n", s.position)
+
+	return
+
+	// Absolutely atrocious attempt to do this algorithmically... keep it simple, stupid!
+	start := s.position
 
 	if right {
 		s.position += ticks
@@ -38,14 +73,10 @@ func solve(s *Solution, l string) {
 
 	fmt.Printf("Preround position: %d\n", s.position)
 
-	revolutions := 0
-	revolutions = int(math.Floor(float64(s.position) / 100))
-	if revolutions < 0 {
-		revolutions = -revolutions
-	}
-
-	s.zeroes += revolutions
-	fmt.Printf("Zeroes: %d\n", s.zeroes)
+	// edge case 1: old pointer at 0, move left, gets counted
+	// 2: new pointer at 0, but moved 99 ticks
+	revolutions := 0.0
+	revolutions = math.Floor(math.Abs(float64(s.position)) / 100)
 
 	if s.position > 99 {
 		s.position = s.position % 100
@@ -58,6 +89,20 @@ func solve(s *Solution, l string) {
 			s.position = -s.position
 		}
 	}
+
+	if start != 0 && s.position < 0 {
+		revolutions += 1
+	}
+
+	if s.position == 0 {
+		revolutions += 1
+	}
+
+	s.zeroes += int(revolutions)
+
+	fmt.Printf("Rounded rev: %d\n", int(revolutions))
+
+	fmt.Printf("Zeroes: %d\n", s.zeroes)
 
 	fmt.Printf("End Position: %d\n", s.position)
 }
