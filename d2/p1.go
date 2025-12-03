591f0c5d013e7b6a41a20becddc7fb4d4d5550e4
Thomas <hi@thomasng.me>
2025-12-03 00:58:31 +0000

    Day 2 progress

--- d2/p1.go
+++ d2/p1.go
@@ -11,13 +11,12 @@ import (
 )
 
 type Solution struct {
+	invalids []int
 }
 
 func solve(s *Solution, l string) {
 	// Remove trailing comma
 	l = l[:len(l)-1]
-
-	fmt.Printf("%s\n", l)
 
 	fromTo := strings.Split(l, "-")
 
@@ -39,7 +38,45 @@ func solve(s *Solution, l string) {
 		if i > to {
 			break
 		}
+
+		str := strconv.Itoa(i)
+
+		repeat := isAllRepeat(str)
+		if repeat {
+			s.invalids = append(s.invalids, i)
+			fmt.Printf("%d\n", i)
+			i++
+			continue
+		}
+
+		if str[:len(str)/2] == str[len(str)/2:] {
+			s.invalids = append(s.invalids, i)
+			fmt.Printf("%d\n", i)
+		}
+
+		i++
 	}
+}
+
+func isAllRepeat(s string) bool {
+	isSame := true
+	if len(s) == 1 {
+		return true
+	}
+
+	for i := range len(s) - 1 {
+		if s[i] == s[i+1] {
+			continue
+		}
+		isSame = false
+		break
+	}
+
+	if len(s)%2 == 1 {
+		return false
+	}
+
+	return isSame
 }
 
 func main() {
@@ -68,4 +105,10 @@ func main() {
 	}
 
 	// fmt.Printf("Solution\n", sln.)
+	n := 0
+	for i := 0; i < len(sln.invalids); i++ {
+		n += sln.invalids[i]
+	}
+
+	fmt.Printf("Final sum: %d\n", n)
 }
